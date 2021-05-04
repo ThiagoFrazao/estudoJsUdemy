f38e64fb33aee4c6598424a20ca1f7c8df3f72ab
Thiago da Costa Pinto Frazão de Sousa <f514189@corp.caixa.gov.br>
2021-05-03 21:01:08 -0300

    setando porta dinamicamente

--- cursoJsGeral/nodeEstudo/projetos/expressApi/src/servidor.js
+++ cursoJsGeral/nodeEstudo/projetos/expressApi/src/servidor.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cool = require('cool-ascii-faces')
 const app = express()
-const porta = 5000
+const porta = process.env.PORT || 5000
 
 let produtos = [
     {
