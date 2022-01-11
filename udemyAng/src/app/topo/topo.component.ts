e8b371c5da6095f28da75db5c40cbdad902bc4ad
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2022-01-10 21:25:56 -0300

    centralizando tela derrota

--- udemyAng/src/app/topo/topo.component.ts
+++ udemyAng/src/app/topo/topo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 //E possivel utilizar tbm o atributo template e definir o html diretamente na marcacao 
 @Component({
@@ -6,8 +6,21 @@ import { Component } from "@angular/core";
     templateUrl: './topo.component.html',
     styleUrls: ['./topo.component.css']
 })
-export class TopoComponent {
+export class TopoComponent implements OnChanges {
     public titulo: string = 'Aprendendo Inglês na Udemy';
+    @Input("mudarLado") public naoMudarLado : boolean;
+    public centralizar : string;
 
+    constructor() {
+        this.naoMudarLado = true;
+        this.centralizar = "";
+    }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        if(this.naoMudarLado) {
+            this.centralizar = "";
+        } else {
+            this.centralizar = "centralizar";
+        }        
+    }    
 }
